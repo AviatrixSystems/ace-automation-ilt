edb2cf8516519ba3f63a4f9f0834c0ef9f050e10
John Smoker <jsmoker@aviatrix.com>
2024-12-09 15:03:19 -0800

    remove obsolete dcf deny

--- dcf/main.tf
+++ dcf/main.tf
@@ -153,21 +153,6 @@ resource "aviatrix_distributed_firewalling_policy_list" "dcf" {
     ]
   }
 
-  policies {
-    name                     = "default-deny-all"
-    action                   = "DENY"
-    priority                 = 2147483646
-    protocol                 = "Any"
-    logging                  = true
-    watch                    = false
-    exclude_sg_orchestration = true
-    src_smart_groups = [
-      "def000ad-0000-0000-0000-000000000000" # Anywhere
-    ]
-    dst_smart_groups = [
-      "def000ad-0000-0000-0000-000000000000" # Anywhere
-    ]
-  }
   depends_on = [
     aviatrix_distributed_firewalling_config.enable
   ]
